6ea55dcdb132c41b72891ec7d07aa5a4448ac8c5
BlaineOb <blnob567@gmail.com>
2025-07-25 17:37:18 -0400

    Add service worker

---
+++ public/sw.js
@@ -0,0 +1,53 @@
+const VERSION = "1.0";
+const CACHE_NAME = `brew-tool-${VERSION}`;
+const APP_STATIC_RESOURCES = [
+  "/",
+  "/beer_big_icon.png",
+  "/beer_medium_icon.png",
+  "/favicon.png",
+  "/manifest.json",
+];
+
+self.addEventListener("install", (e) => {
+  e.waitUntil(
+    (async () => {
+      const cache = await caches.open(CACHE_NAME);
+      cache.addAll(APP_STATIC_RESOURCES);
+    })()
+  );
+});
+
+self.addEventListener("activate", (e) => {
+  e.waitUntil(
+    (async () => {
+      const names = await caches.keys();
+      await Promise.all(
+        names.map((name) => {
+          if (name !== CACHE_NAME) {
+            return caches.delete(name);
+          }
+        })
+      );
+      await clients.claim();
+    })()
+  );
+});
+
+self.addEventListener("fetch", (e) => {
+  if (
+    !(e.request.url.startsWith("http:") || e.request.url.startsWith("https:"))
+  ) {
+    return;
+  }
+
+  e.respondWith(
+    (async () => {
+      const r = await caches.match(e.request);
+      if (r) return r;
+      const response = await fetch(e.request);
+      const cache = await caches.open(cacheName);
+      cache.put(e.request, response.clone());
+      return response;
+    })()
+  );
+});
